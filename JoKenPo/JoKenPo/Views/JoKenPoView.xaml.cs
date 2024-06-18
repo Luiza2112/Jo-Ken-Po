32749e8080745b1325a57c89a982b7e8dbbb621d
Luiza2112 <ritafilhotes4@gmail.com>
2024-06-18 19:50:13 -0300

    Início da View e adição da pasta Model e ViewModels.

---
+++ JoKenPo/JoKenPo/Views/JoKenPoView.xaml.cs
@@ -0,0 +1,14 @@
+namespace JoKenPo.Views;
+
+public partial class JoKenPoView : ContentView
+{
+	public JoKenPoView()
+	{
+		InitializeComponent();
+	}
+
+    private void JokenPoButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
